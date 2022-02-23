7f0f438ede97e42539568a787122e59dcdd02c69
Ben Mechen <ben@chelsea-apps.com>
2022-02-23 17:50:03 +0000

    feat(system): Add modal to update systems

--- src/pages/main/Dashboard/components/SystemCard/SystemCard.tsx
+++ src/pages/main/Dashboard/components/SystemCard/SystemCard.tsx
@@ -1,7 +1,9 @@
+import { useModal } from '../../../../../components/Modal';
 import { OptionsButton } from '../../../../../components/OptionsButton';
 import { Card } from '../Card';
 import { Deployment } from './components/Deployment';
 import { Status } from './components/Status';
+import { UpdateSystemForm } from './components/UpdateSystemForm';
 
 export interface ISystemCard {
 	name: string;
@@ -15,18 +17,27 @@ export interface ISystemCard {
 	}[];
 }
 
-const SystemCard = ({ name, status, deployments }: ISystemCard) => (
-	<Card>
-		<div className="flex w-full justify-between items-start">
-			<Status {...status} />
-			<OptionsButton />
-		</div>
-		<h2 className="text-3xl font-light">{name}</h2>
-		<div className="max-w-full overflow-x-scroll flex gap-2 no-scrollbar">
-			{deployments.map((deployment, index) => (
-				<Deployment index={index} {...deployment} />
-			))}
-		</div>
-	</Card>
-);
+const SystemCard = ({ name, status, deployments }: ISystemCard) => {
+	const [Modal, { open }] = useModal();
+
+	return (
+		<>
+			<Modal title={name}>
+				<UpdateSystemForm name={name} onSubmit={() => {}} />
+			</Modal>
+			<Card>
+				<div className="flex w-full justify-between items-start">
+					<Status {...status} />
+					<OptionsButton onClick={open} />
+				</div>
+				<h2 className="text-3xl font-light">{name}</h2>
+				<div className="max-w-full overflow-x-scroll flex gap-2 no-scrollbar">
+					{deployments.map((deployment, index) => (
+						<Deployment index={index} {...deployment} />
+					))}
+				</div>
+			</Card>
+		</>
+	);
+};
 export default SystemCard;
