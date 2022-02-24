016778b5c3087776d904f31a095a07b796f50a69
Ben Mechen <ben@chelsea-apps.com>
2022-02-24 08:34:18 +0000

    feat(deployments): Add deployment create and update modals

--- src/pages/main/System/components/CreateDeploymentButton/CreateDeploymentButton.tsx
+++ src/pages/main/System/components/CreateDeploymentButton/CreateDeploymentButton.tsx
@@ -1,14 +1,27 @@
 import { Plus } from 'phosphor-react';
+import { useModal } from '../../../../../components/Modal';
+import { DeploymentForm } from '../Deployment/components/DeploymentForm';
 
-const CreateDeploymentButton = () => (
-	<button
-		className="rounded-l-full w-20 h-20 bg-gradient-to-r from-cyan-500 to-blue-500 fixed p-5 flex transition-transform duration-150 hover:-translate-x-1"
-		style={{
-			right: '-2rem',
-			top: '50%',
-		}}
-	>
-		<Plus color="white" width="100%" className="-translate-x-3" />
-	</button>
-);
+const CreateDeploymentButton = () => {
+	const [Modal, { open }] = useModal();
+
+	return (
+		<>
+			<Modal title="Create Deployment">
+				<DeploymentForm type="create" onSubmit={() => {}} />
+			</Modal>
+			<button
+				className="rounded-l-full w-20 h-20 bg-gradient-to-r from-cyan-500 to-blue-500 fixed p-5 flex transition-transform duration-150 hover:-translate-x-1"
+				style={{
+					right: '-2rem',
+					top: '50%',
+				}}
+				onClick={open}
+			>
+				<Plus color="white" width="100%" className="-translate-x-3" />
+			</button>
+		</>
+	);
+};
+
 export default CreateDeploymentButton;
---
+++ src/pages/main/System/components/Deployment/components/DeploymentForm/DeploymentForm.tsx
@@ -0,0 +1,118 @@
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Button } from '../../../../../../../components/Button';
+import { Input } from '../../../../../../../components/Input';
+import { Platform as PlatformSelector } from './components/Platform';
+
+enum Platform {
+	AWS,
+	AZURE,
+	GCP,
+	OTHER,
+}
+
+interface IDeploymentFormData {
+	name: string;
+	aws_id?: string;
+	aws_secret?: string;
+	aws_region?: string;
+	azure_tenantId?: string;
+	azure_clientId?: string;
+	azure_secret?: string;
+}
+
+interface IDeploymentForm {
+	type: 'create' | 'update';
+	name?: string;
+	onSubmit: SubmitHandler<IDeploymentFormData>;
+}
+const DeploymentForm = ({ type, name, onSubmit }: IDeploymentForm) => {
+	const [platform, setPlatform] = useState<Platform>(Platform.AWS);
+	const { handleSubmit, register } = useForm<IDeploymentFormData>({
+		defaultValues: {
+			name,
+		},
+	});
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+			<Input
+				placeholder="Name"
+				{...register('name', { required: true })}
+			/>
+			<div className="rounded-lg bg-gray-100 w-full h-12 flex items-center justify-around overflow-hidden">
+				<PlatformSelector
+					selected={platform === Platform.AWS}
+					onClick={() => setPlatform(Platform.AWS)}
+				>
+					AWS
+				</PlatformSelector>
+				<PlatformSelector
+					selected={platform === Platform.AZURE}
+					onClick={() => setPlatform(Platform.AZURE)}
+				>
+					Azure
+				</PlatformSelector>
+				<PlatformSelector
+					selected={platform === Platform.GCP}
+					onClick={() => setPlatform(Platform.GCP)}
+				>
+					GCP
+				</PlatformSelector>
+				<PlatformSelector
+					selected={platform === Platform.OTHER}
+					onClick={() => setPlatform(Platform.OTHER)}
+				>
+					Other
+				</PlatformSelector>
+			</div>
+			{platform === Platform.AWS ? (
+				<>
+					<Input
+						placeholder="Access Key ID"
+						{...register('aws_id', { required: true })}
+					/>
+					<Input
+						placeholder="Access Key Secret"
+						type="password"
+						{...register('aws_secret', { required: true })}
+					/>
+					<Input
+						placeholder="Region (eu-west-2)"
+						{...register('aws_region', { required: true })}
+					/>
+				</>
+			) : platform === Platform.AZURE ? (
+				<>
+					<Input
+						placeholder="Tenant ID"
+						{...register('azure_tenantId', { required: true })}
+					/>
+					<Input
+						placeholder="Client ID"
+						{...register('azure_clientId', { required: true })}
+					/>
+					<Input
+						placeholder="Secret"
+						type="password"
+						{...register('azure_secret', { required: true })}
+					/>
+				</>
+			) : (
+				<>
+					<Input
+						placeholder="Credential"
+						{...register('aws_id', { required: true })}
+					/>
+				</>
+			)}
+			{type === 'create' ? (
+				<Button>Create Deployment</Button>
+			) : (
+				<Button>Save Changes</Button>
+			)}
+		</form>
+	);
+};
+
+export default DeploymentForm;
